606bd480dc4b8bfada34f6c244dcdb4d4d447b94
Amir <amirbehvandi747@gmail.com>
2021-04-24 10:46:59 +0430

    working on events date

--- mysite/training/models.py
+++ mysite/training/models.py
@@ -4,40 +4,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-class CandidateProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    customer_id = models.CharField(max_length=30, null=True, blank=True )
-    passport_id = models.CharField(max_length=30, null=True, blank=True )
-    twi_candidate_id = models.CharField(max_length=30, null=True, blank=True )
-    sponsor_company = models.CharField(max_length=30, null=True, blank=True )
-    email = models.EmailField( null=True, blank=True )
-    city = models.CharField(max_length=30,  null=True, blank=True  )
-    country = models.CharField(max_length=30,  null=True, blank=True  )
-    contact_number = models.CharField(max_length=30,  null=True, blank=True  )
-    birth_date = models.DateField(null=True, blank=True)
-    photo = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_1 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_2 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_3 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_4 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_5 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_6 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_7 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_8 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    document_9 = models.FileField(upload_to='candidate_document',null=True,blank=True)
-    avatar = models.FileField(upload_to='avatar',null=True,blank=True)
-    note = models.TextField(max_length=2000,null=True,blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    # def __str__(self):
-    #     return self.first_name + ' ' + self.last_name
-@receiver(post_save, sender=User)
-
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        CandidateProfile.objects.create(user=instance)
-    instance.candidateprofile.save()
 
 
 class Product(models.Model):
@@ -79,11 +45,49 @@ class Event(models.Model):
     product = models.ForeignKey(Product,related_name="product_event",  null=True, blank=True , on_delete=models.CASCADE)
     country = models.ForeignKey(Country,related_name="country_event",  null=True, blank=True , on_delete=models.CASCADE)
     location = models.ForeignKey(Location,related_name="location_event",  null=True, blank=True , on_delete=models.CASCADE)
-    start_date = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    # start_date = models.DateField(null=True, blank=True)
 
-    announcement_type =  models.CharField(max_length=1, choices=ANNOUNCMENT_CHOICES)
+    announcement_type =  models.CharField(max_length=1,null=True, blank=True, choices=ANNOUNCMENT_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class CandidateProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    customer_id = models.CharField(max_length=30, null=True, blank=True )
+    passport_id = models.CharField(max_length=30, null=True, blank=True )
+    twi_candidate_id = models.CharField(max_length=30, null=True, blank=True )
+    sponsor_company = models.CharField(max_length=30, null=True, blank=True )
+    email = models.EmailField( null=True, blank=True )
+    city = models.CharField(max_length=30,  null=True, blank=True  )
+    country = models.CharField(max_length=30,  null=True, blank=True  )
+    contact_number = models.CharField(max_length=30,  null=True, blank=True  )
+    birth_date = models.DateField(null=True, blank=True)
+    events = models.ManyToManyField('Event')
+    photo = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_1 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_2 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_3 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_4 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_5 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_6 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_7 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_8 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    document_9 = models.FileField(upload_to='candidate_document',null=True,blank=True)
+    avatar = models.FileField(upload_to='avatar',null=True,blank=True)
+    note = models.TextField(max_length=2000,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):
+    #     return self.first_name + ' ' + self.last_name
+@receiver(post_save, sender=User)
+
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        CandidateProfile.objects.create(user=instance)
+    instance.candidateprofile.save()
