6582b10a245271814e51129047ac6edeedc87a0a
Amir <amirbehvandi747@gmail.com>
2022-01-28 19:47:04 +0330

    updating timesheet data insertion

--- mysite/timesheet/urls.py
+++ mysite/timesheet/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 
     path('stafftimesheetlist/<int:id>', views.TimesheetList.as_view(), name='stafftimesheetlist_'),
     path('newtimesheet/', views.NewTimesheetForm.as_view(), name='newtimesheet_'),
+    path('timesheetcal/', views.TimesheetCalendarView.as_view(), name='timesheetcal_'),
     path('adminstafftimesheet/', views.AdminTimesheetList.as_view(), name='adminstafftimesheet_'),
 
 
--- mysite/timesheet/views.py
+++ mysite/timesheet/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from authorization.sidebarmixin import SidebarMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime
+import json
 # Create your views here.
 
 class TimesheetList(LoginRequiredMixin,SidebarMixin,TemplateView):
@@ -30,6 +31,40 @@ class AdminTimesheetList(LoginRequiredMixin,SidebarMixin,TemplateView):
         timesheets = Timesheet.objects.all()
         context['timesheets'] = timesheets
         return context
+
+
+class TimesheetCalendarView(LoginRequiredMixin,SidebarMixin,TemplateView):
+
+    template_name = "timesheet/apps-calendar.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TimesheetCalendarView, self).get_context_data()
+        timesheets = Timesheet.objects.all()
+        context['timesheets'] = timesheets
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+
+                dateListObj = request.POST['dateList']
+
+                # for item in dateListObj:
+                jsonDate =json.loads(dateListObj)
+                # print(jsonDate)
+
+                for i in jsonDate:
+                    print(i)
+                # for item in dateListObj:
+                #     print(item.title)
+                # obj = Timesheet()
+                # obj.staff =User.objects.filter(id=request.POST['userID']).first()
+                # obj.from_date = datetime.strptime(request.POST['from_date'], '%I:%M %p')
+                # obj.to_date = datetime.strptime(request.POST['to_date'], '%I:%M %p')
+                # obj.description = request.POST['description']
+                #
+                # obj.save()
+
+                return redirect('timesheet:timesheetcal_')
 
 
 
