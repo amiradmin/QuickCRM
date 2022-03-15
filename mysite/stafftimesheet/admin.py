a7ccd093f4acb4bea873d945020c3f333650d676
Amir <amirbehvandi747@gmail.com>
2022-03-15 07:19:11 +0330

    uodate timesheet report

--- mysite/stafftimesheet/admin.py
+++ mysite/stafftimesheet/admin.py
@@ -4,7 +4,7 @@ from stafftimesheet.models import Timesheet
 class TimesheetAdmin(admin.ModelAdmin):
 
 
-    list_display = ['id','staff','description','duration','approved','created_at','updated_at']
+    list_display = ['id','staff','description','task','duration','approved','created_at','updated_at']
     list_filter = ['id','staff','description','duration','approved','created_at','updated_at']
 
 admin.site.register(Timesheet,TimesheetAdmin)
