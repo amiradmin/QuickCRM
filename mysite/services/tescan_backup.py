87269234f84ddbaccf5e495d021df657b7cb3ecb
Amir <amirbehvandi747@gmail.com>
2022-04-30 18:27:11 +0430

    writing backup serivce

--- mysite/services/tescan_backup.py
+++ mysite/services/tescan_backup.py
@@ -1,4 +1,6 @@
 import os
 
-os.system('sudo rm -r /home/amir/rep_media')
+path ='/home/amir/rep_media'
+if  os.path.isdir(path):
+    os.system('sudo rm -r /home/amir/rep_media')
 os.system('sshpass -p "q95yPd18npk8fC" scp -r root@185.231.59.78:/home/QuickCRM/mysite/media/ /home/amir/rep_media')
