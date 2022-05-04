173deda1f302cf2d2e1a75bb3dc71835195c6c7f
Amir <amirbehvandi747@gmail.com>
2022-05-04 11:33:35 +0430

    updates settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -150,7 +150,7 @@ else:
             'NAME': 'tesdb',
             'USER': 'tes_dbuser',
             'PASSWORD': "1qaz!QAZ",
-            'HOST': '127.0.0.1',
+            'HOST': '185.231.59.78',
             'PORT': '5432',
         },
 
