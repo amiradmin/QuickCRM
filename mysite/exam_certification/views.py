06bce2bc423bd724188d95b6ac2c0bb6914f7b5b
Amir <amirbehvandi747@gmail.com>
2022-05-31 12:00:32 +0430

    update profile

--- mysite/exam_certification/views.py
+++ mysite/exam_certification/views.py
@@ -312,7 +312,7 @@ class ExamResultHistoryCSWIP31(SidebarMixin, LoginRequiredMixin, TemplateView):
         # overall_porpose = results.first()
         # print(overall_porpose)
         # if overall_porpose.general_paper == str('Passed') & overall_porpose.technology_paper == 'Passed' & overall_porpose.plate_paper == 'Passed' & overall_porpose.pipe_paper == 'Passed' & overall_porpose.macro_paper == 'Passed':
-        if overall_porpose.general_paper == 'Passed' :
+        if results.general_paper == 'Passed' :
             print('Here')
             results.overall = 'Passed'
         else :
