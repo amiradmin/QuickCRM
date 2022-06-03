308da0ce78529e1db0c5b3490411434edc08b1c0
Amir <amirbehvandi747@gmail.com>
2022-06-03 08:17:12 +0430

    updating candidate request url

--- mysite/training/urls.py
+++ mysite/training/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path('addformcategory/<int:id>', views.AddFormToCategoryView.as_view(), name='addformcategory_'),
 
 
-    path('requestregister/<int:id>/<int:eventID>', views.RequestRegister.as_view(), name='requestregister_'),
+    path('requestregister/<int:id>/<int:eventID>/', views.RequestRegister.as_view(), name='requestregister_'),
 
 
 ]
