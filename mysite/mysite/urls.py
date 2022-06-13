b4bebeabefd698507bd66beb53c5149d39ec319e
Amir <amirbehvandi747@gmail.com>
2022-06-13 13:21:16 +0430

    update password reset

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
 
     path('accounts/', include('django.contrib.auth.urls')),
     path('reset_password/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+
     path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
