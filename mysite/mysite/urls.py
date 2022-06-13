886b7570c94499118f34d5ac55bfa8a5fdba8323
Amir <amirbehvandi747@gmail.com>
2022-06-13 15:10:42 +0430

    update password reset

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     # path('reset_password/',auth_views.PasswordResetView.as_view(),name='password_reset'),
-    # path('accounts/password-reset/', auth_views.PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html'), name='password_reset'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html'), name='password_reset'),
     path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
