cb9a74b2aabb9af65ba4b4c59473325a409adad1
Amir <amirbehvandi747@gmail.com>
2022-08-02 03:11:15 +0430

    update user statistics

--- mysite/monitoring/views.py
+++ mysite/monitoring/views.py
@@ -7,6 +7,7 @@ from django.views.generic import View, TemplateView
 from django.contrib.auth.models import User,Group
 from monitoring.models import UserMonitor,LastLogin
 from django.db.models import Q, Count
+from training.models import Event,TesCandidate
 import datetime
 
 class UserStatistics(GroupRequiredMixin,LoginRequiredMixin,SidebarMixin, TemplateView):
@@ -22,7 +23,7 @@ class UserStatistics(GroupRequiredMixin,LoginRequiredMixin,SidebarMixin, Templat
         total_candidate = TesCandidate.objects.all().count()
         five_minutes_ago = datetime.datetime.now() + datetime.timedelta(minutes=-5)
         online_users = UserMonitor.objects.filter(login_date__gte=five_minutes_ago).count()
-        last_login = LastLogin.objects.all()
+        last_login = LastLogin.objects.order_by('user','-login').distinct('user')
         login_number = LastLogin.objects\
     .all()\
     .values('user')\
@@ -34,6 +35,26 @@ class UserStatistics(GroupRequiredMixin,LoginRequiredMixin,SidebarMixin, Templat
             print(item)
             user= User.objects.filter(id=item['user']).first()
             last_user_list.append(user.username)
+
+
+        date=datetime.datetime.strptime('2021-01-01', "%Y-%m-%d").date()
+        print(date)
+        candidates_id = Event.objects.filter(start_date__gte = date).values_list('candidate', flat=True)
+        emails = TesCandidate.objects.filter(id__in = [candidates_id]).values_list('email',flat=True)
+        import xlsxwriter
+        row = 0
+        column = 0
+
+
+        workbook = xlsxwriter.Workbook('hello.xlsx')
+        worksheet = workbook.add_worksheet()
+
+
+        for item in emails:
+            print(item)
+            worksheet.write(row, column, item)
+            row += 1
+        workbook.close()
 
         context['group_name'] = group_name
         context['candidate'] = candidate
