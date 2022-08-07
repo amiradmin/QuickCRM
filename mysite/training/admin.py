ba882c3c8ca07c334dea753d4ff24e63f75fa91a
Amir <amirbehvandi747@gmail.com>
2022-08-07 16:25:28 +0430

    update admin panel

--- mysite/training/admin.py
+++ mysite/training/admin.py
@@ -31,7 +31,7 @@ admin.site.register(CandidateProfile,CandidateProfileAdmin)
 class TesCandidateAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 
 
-    list_display = ['id','first_name','customer_id','tes_candidate_id','email','photo','document_1','document_2','contact_number','contact_number','birth_date','created_at','updated_at']
+    list_display = ['id','user','first_name','customer_id','tes_candidate_id','email','photo','document_1','document_2','contact_number','contact_number','birth_date','created_at','updated_at']
     list_filter = ['id','first_name','customer_id','tes_candidate_id','email','contact_number','birth_date','created_at','updated_at']
 
 admin.site.register(TesCandidate,TesCandidateAdmin)
