f482bc02d6b7b66aa54f88681b4d1348d2234531
Amir <amirbehvandi747@gmail.com>
2022-08-14 14:16:31 +0430

    update new candidate

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
