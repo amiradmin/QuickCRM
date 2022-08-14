bd7b92197901c5a021e4fbc46dc765e2e851513b
Amir <amirbehvandi747@gmail.com>
2022-08-14 14:55:57 +0430

    update new candidate

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,11 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import request
+import
 from mysite.settings.development import *
 from django.core.wsgi import get_wsgi_application
-urlconf = getattr(request, "urlconf",mysite.settings.development.ROOT_URLCONF)
-print(urlconf)
+
+
+print(ROOT_URLCONF)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
 # os.environ["DJANGO_SETTINGS_MODULE"] = 'mysite.settings.development'
 
