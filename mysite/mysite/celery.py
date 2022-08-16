46057c53158faf4070bc5ddb3bab6a0bc48b19c2
Amir <amirbehvandi747@gmail.com>
2022-08-16 10:37:49 +0430

    update settings

--- mysite/mysite/celery.py
+++ mysite/mysite/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.production')
 
 app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
