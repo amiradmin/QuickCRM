57c53f9e134e0afa7af0154056bbadbcc4a57a3f
Amir <amirbehvandi747@gmail.com>
2022-08-18 08:48:42 +0430

    updateing settings

--- mysite/mysite/settings/common.py
+++ mysite/mysite/settings/common.py
@@ -16,9 +16,9 @@ import platform
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-
+print(TEMPLATE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
