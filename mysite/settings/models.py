950c74fb5907d54762faac6811f69b1331dff9f4
Amir <amirbehvandi747@gmail.com>
2022-09-27 06:43:00 -0400

    update from server

--- mysite/settings/models.py
+++ mysite/settings/models.py
@@ -8,6 +8,7 @@ class SubMenuLevelTwo(models.Model):
     code = models.CharField(max_length=512, null=True, blank=True )
     url = models.CharField(max_length=512, null=True, blank=True )
 
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,8 +16,8 @@ class SubMenuLevelTwo(models.Model):
         return self.title
 
 class SubMenuLevelOne(models.Model):
-    group = models.ManyToManyField(SubMenuLevelTwo,  null=True, blank=True)
-    submenu = models.ManyToManyField(Group,  null=True, blank=True)
+    group = models.ManyToManyField(Group,  null=True, blank=True)
+    submenu = models.ManyToManyField(SubMenuLevelTwo,  null=True, blank=True)
     title = models.CharField(max_length=512, null=True, blank=True )
     code = models.CharField(max_length=512, null=True, blank=True )
     url = models.CharField(max_length=512, null=True, blank=True )
@@ -32,7 +33,7 @@ class Sidebar(models.Model):
     group = models.ManyToManyField(Group,  null=True, blank=True)
     submenu = models.ManyToManyField(SubMenuLevelOne,  null=True, blank=True)
     title = models.CharField(max_length=512, null=True, blank=True )
-
+    href = models.CharField(max_length=512, null=True, blank=True )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
