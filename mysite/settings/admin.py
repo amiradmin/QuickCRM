95a0d0b6e2bd6bd57fa412c990b4d8d705004216
Amir <amirbehvandi747@gmail.com>
2022-09-27 13:38:38 -0400

    update dynamic menu bar

--- mysite/settings/admin.py
+++ mysite/settings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from settings.models import Sidebar,SubMenuLevelOne,SubMenuLevelTwo
+from settings.models import Sidebar,SubMenuLevelOne,SubMenuLevelTwo,SubMenuLevelThree
 # Register your models here.
 class SidebarAdmin(admin.ModelAdmin):
 
@@ -19,3 +19,9 @@ class SubMenuLevelTwoAdmin(admin.ModelAdmin):
     list_display = ['id','title','created_at','updated_at']
     list_filter = ['id','title','created_at','updated_at']
 admin.site.register(SubMenuLevelTwo,SubMenuLevelTwoAdmin)
+
+class SubMenuLevelThreeAdmin(admin.ModelAdmin):
+
+    list_display = ['id','title','created_at','updated_at']
+    list_filter = ['id','title','created_at','updated_at']
+admin.site.register(SubMenuLevelThree,SubMenuLevelThreeAdmin)
